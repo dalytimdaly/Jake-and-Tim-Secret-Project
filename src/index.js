c9d7004cfe34f6339b11f3779461996cc23582f9
Tim <dalytimdaly@gmail.com>
2022-08-19 12:48:19 -0600

    fixed js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ function general_fetch(url) {
     return fetch(url).then(res => { return res.json() })
 }
 
-<<<<<<< HEAD
-general_fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY").then((data) => {
-=======
 general_fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY').then((data) => {
->>>>>>> 8e95b03172741a5074e6d23d2c73a1087bd50119
     console.log(data)
 
     nasaData = data.photos
