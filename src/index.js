13389e2c5e09764f64d4ff0efd72f4ce031d591e
Tim <dalytimdaly@gmail.com>
2022-08-22 09:22:30 -0600

    updated html

--- src/index.js
+++ src/index.js
@@ -4,22 +4,65 @@ function general_fetch(url) {
     return fetch(url).then(res => { return res.json() })
 }
 
-general_fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY').then((data) => {
+general_fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=JKPUIJZAatGpCWnlTbfIK1WVS7sOxcMqERcI78jJ').then((data) => {
     console.log(data)
 
     nasaData = data.photos
+    console.log(nasaData[0])
+    
+    let currentImage = document.getElementById('current-image')
 
-    nasaData.forEach(photo => {
-        createImage(photo)
+    // set this to randomly generate image from data set so that it's always different
+
+    function createImage() {
+        currentImage.src = nasaData[0].img_src
+    }
+
+    createImage()
+
+    let displayId = document.getElementById('image-id')
+
+    displayId.textContent = `CURRENT IMAGE: #${nasaData[0].id}`
+
+    // comment section
+
+    let post = document.getElementById('comment-form')
+    let commentSection = document.querySelector('#comments-section')
+
+    document.addEventListener('submit', (event) => {
+        event.preventDefault()
+        let newComment = document.createElement('p')
+        newComment.textContent = post['commentInput'].value
+        commentSection.appendChild(newComment)
+
+    })  
+
+    // favorite image
+
+    let favorite = document.getElementById('favorite-button')
+    let favoriteCount = document.getElementById('number-favorites')
+    let count = 1
+
+    favorite.addEventListener('click', (event) => {
+        event.preventDefault()
+        console.log("hi")
+        favoriteCount.textContent = `${count ++} FAVORITES`
 
     })
-
-   
+    
+    
 })
 
-function createImage(photo) {
-    let paragraph = document.getElementById("p1")
-    let img = document.createElement('img')
-    img.src = photo.img_src
-    paragraph.append(img);
-}
+/*
+    function appendFavorite() {
+        let duckList = document.querySelector('#duck-nav')
+        let img = document.createElement('img')
+        img.src = duck.img_url
+        duckList.appendChild(img);
+        img.addEventListener('click', () => {
+            loadDuck()
+        })
+    }
+
+
+*/
